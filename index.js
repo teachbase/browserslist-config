74ee94fb1dfa88c3e3a31da804fb7a5434d8b82d
Alexander Telyatnikov <labriko@yandex.ru>
2018-05-17 18:41:59 +0300

    feat: Export browserslist

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = [
+  "> 1% in RU",
+  "Safari >= 11.1",
+  "iOS >= 11.3",
+  "Edge >= 17",
+  "not Safari <= 11.0",
+  "not iOS <= 11.2",
+  "not IE <= 11",
+  "not Edge <= 16"
+];
